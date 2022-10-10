da67c2e36a2a94cc7b22589455a3bf4352432fd5
zerkodv <s3937120@student.rmit.edu.au>
2022-10-10 18:31:25 +1100

    added dependencies and controller

---
+++ Account/src/main/java/com/example/account/controller/accountController.java
@@ -0,0 +1,5 @@
+package com.example.account.controller;
+
+public class accountController {
+
+}
--- Account/src/main/java/com/example/account/dao/AccountDatabase.java
+++ Account/src/main/java/com/example/account/dao/AccountDatabase.java
@@ -1,5 +1,10 @@
 package com.example.account.dao;
 
-public class AccountDatabase {
-    
+import com.example.account.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountDatabase extends JpaRepository<Account, Long> {
+
 }
