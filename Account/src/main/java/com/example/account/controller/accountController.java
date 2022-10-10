4913cf976e303134f3736b096b4447d9a4df6bf3
zerkodv <s3937120@student.rmit.edu.au>
2022-10-10 18:33:05 +1100

    updated controller

--- Account/src/main/java/com/example/account/controller/accountController.java
+++ Account/src/main/java/com/example/account/controller/accountController.java
@@ -1,5 +1,14 @@
 package com.example.account.controller;
 
-public class accountController {
 
+import com.example.account.dao.AccountDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class accountController {
+    @Autowired
+    AccountDatabase accountDatabase;
+
+    
 }
