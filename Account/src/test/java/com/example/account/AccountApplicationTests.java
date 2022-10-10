423abcf2fd47c8281a307ad5eaa928c394f94d74
zerkodv <s3937120@student.rmit.edu.au>
2022-10-11 01:43:31 +1100

    junit testing added

--- Account/src/test/java/com/example/account/AccountApplicationTests.java
+++ Account/src/test/java/com/example/account/AccountApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.account;
 
+import com.example.account.controller.AccountController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class AccountApplicationTests {
 
+    @Autowired
+    AccountController accountController;
+
     @Test
     void contextLoads() {
+        Assertions.assertAll((Executable) accountController);
     }
 
 }
---
+++ Account/src/test/java/com/example/account/ControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.account;
+
+import com.example.account.controller.AccountController;
+import com.example.account.dao.AccountDatabase;
+import com.example.account.model.Account;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.reflect.Array.get;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(AccountController.class)
+public class ControllerTest {
+
+    @MockBean
+    AccountDatabase accountDatabase;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testfindAll() throws Exception {
+        Account account = new Account(03L,"Loan","23456789","Johns Smith", "300", "2022-10-10");
+        List<Account> accounts = Arrays.asList(account);
+
+
+
+        Mockito.when(accountDatabase.findAll()).thenReturn(accounts);
+
+        mockMvc.perform((RequestBuilder) get("/persons",1))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$", Matchers.hasSize(1)))
+                .andExpect((ResultMatcher) jsonPath("$[0].name", Matchers.is("John Book")));
+    }
+
+
+}
